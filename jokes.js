5969ed8a7dba0de9ee73759143cbd7f68b7c24bd
523ChloeL <92735830+523ChloeL@users.noreply.github.com>
2022-03-07 08:20:03 -0500

    Add files via upload

--- jokes.js
+++ jokes.js
@@ -1,10 +1,16 @@
 var jokeArray = {
-
+  'What do you call a penguin in the White House?'
+  'What do you call a man with no arms or legs, laying in front of your door?'
 }
 var answerArray = {
-
+  'Lost'
+  'Matt'
 }
-
+var jokeNumber = 	Math.floor(Math.random() * jokeArray.length);
+	document.getElementById('joke').textContent = (jokeArray[randomNumber]).value;
 function answer() {
-  document.getElementById("answer").innerHTML = "Matt"
+  document.getElementById('answer').textContent = answerArray[jokeNumber];
+}
+function newJoke() {
+
 }
