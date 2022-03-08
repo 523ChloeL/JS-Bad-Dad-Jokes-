6cf5785110363b9a0595ff40f93896ed7fc5b57d
523ChloeL <92735830+523ChloeL@users.noreply.github.com>
2022-03-08 10:43:28 -0500

    image array and background issues

--- jokes.js
+++ jokes.js
@@ -1,16 +1,26 @@
 var jokeArray = {
-  'What do you call a penguin in the White House?'
+  'What do you call a penguin in the White House?',
   'What do you call a man with no arms or legs, laying in front of your door?'
 }
 var answerArray = {
-  'Lost'
+  'Lost',
   'Matt'
 }
-var jokeNumber = 	Math.floor(Math.random() * jokeArray.length);
-	document.getElementById('joke').textContent = (jokeArray[randomNumber]).value;
+var newJoke = 	Math.floor(Math.random() * jokeArray.length);
+	document.getElementById('joke').textContent = (jokeArray[newJoke].value);
 function answer() {
-  document.getElementById('answer').textContent = answerArray[jokeNumber];
+  document.getElementById('answer').textContent = answerArray[newJoke-1];
 }
-function newJoke() {
+var IsCalledOnce = false;
 
+function master()
+{
+ if(IsCalledOnce)
+{
+  newJoke();
+}else
+{
+ answer();
+}
+IsCalledOnce = true;
 }
---
+++ meme.js
@@ -0,0 +1,10 @@
+var myPix = new Array("images/lion.jpg", "images/tiger.jpg", "images/bear.jpg");
+var randomMeme = 	Math.floor(Math.random() * memeArray.length);
+	document.getElementById('press-quote').textContent = '“' + quotesArray[randomNumber].split(' — ')[0] + '” — ' + quotesArray[randomNumber].split(' — ')[1];
+function newMeme() {
+  //change background to toy
+  document.getElementById('toy-pic').style.backgroundImage = 'url(' + toys.value + '.jpg)';
+  //document.getElementById('toy-desc').style.text = toys.selectedIndex;
+
+  document.getElementById('toy-desc').textContent = descArray[desIndex-1];
+}
